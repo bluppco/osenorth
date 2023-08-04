253b0e071b79868b3451998768ac09134d2f7697
Mohit <mohit@hey.com>
2023-08-04 11:07:11 +0530

    x

---
+++ src/constants/index.js
@@ -0,0 +1,125 @@
+export const WORK =  [
+
+    {
+
+        title: "OnePort — Starlight Theme",
+        image: "",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+            "DEVELOPMENT"
+
+        ]
+
+    },
+    {
+
+        image:"",
+        title:"KOBP — Paranormal Investigation Website",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+            "NO-CODE"
+
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "KOBP — Paranormal Investigation Website",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+            "NO-CODE"
+
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "Twitter Bot Website Design",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "Krauf Portfolio",
+        categories: [
+
+            "DESIGN",
+            "DEVELOPMENT"
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "MellowVillas — Staycation Website",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+            "DEVELOPMENT"
+
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "Minecraft Server",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "Urbano — Digital Agency Website",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "Joseph Portfolio",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+
+        ]
+
+    },
+    {
+
+        image: "",
+        title: "Ahoikapptn! — Agency Website",
+        categories: [
+
+            "BRANDING",
+            "DESIGN",
+        ]
+
+    }
+
+]
